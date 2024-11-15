961c9a856b3c2be1a9df364c38f22d682ae0ab2d
Leonardo <leo-a-oliveira@outlook.com>
2024-11-15 03:24:53 -0300

    Ajuste de convenções e aplicação de migrations no modelo de dados

--- WebStyle.ProductApi/Context/AppDbContext.cs
+++ WebStyle.ProductApi/Context/AppDbContext.cs
@@ -9,4 +9,70 @@ public class AppDbContext : DbContext
     
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+
+    //Fluent API 
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        //category
+        mb.Entity<Category>().HasKey(c => c.CategoryId);
+
+         mb.Entity<Category>().
+            Property(c => c.Name).
+              HasMaxLength(100).
+                IsRequired();
+
+        //product
+        mb.Entity<Product>().
+            Property(c => c.Name).
+              HasMaxLength(100).
+                IsRequired();
+
+        mb.Entity<Product>().
+          Property(c => c.Description).
+               HasMaxLength(255).
+                   IsRequired();
+
+        mb.Entity<Product>().
+          Property(c => c.ImageURL).
+              HasMaxLength(255).
+                  IsRequired();
+
+        mb.Entity<Product>().
+          Property(c => c.Price).
+            HasPrecision(12, 2); 
+        
+        mb.Entity<Category>()
+          .HasMany(g => g.Products)
+            .WithOne(c => c.Category)
+            .IsRequired()
+              .OnDelete(DeleteBehavior.Cascade);
+            
+        mb.Entity<Category>().HasData(
+        new Category
+        {
+            CategoryId = 1,
+            Name = "Acessórios",
+        },
+        new Category
+        {
+            CategoryId = 2,
+            Name = "Feminino",
+        },
+        new Category
+        {
+            CategoryId = 3,
+            Name = "Infantil",
+        },
+        new Category
+        {
+            CategoryId = 4,
+            Name = "Masculino"
+        },
+        new Category
+        {
+            CategoryId = 5,
+            Name = "Unissex"
+        }
+        );          
+    }
 }
---
+++ WebStyle.ProductApi/Migrations/20241115055814_SeedProducts.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebStyle.ProductApi.Migrations
+{
+    public partial class SeedProduct : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Products(Name, Price, Description, Stock, ImageUrl, CategoryId) " + 
+                "Values('Camisa',20.55,'Camisa',20,'camisa.jpg',5)");
+
+            mb.Sql("Insert into Products(Name, Price, Description, Stock, ImageUrl, CategoryId) " +
+                "Values('Mochila',26.50,'Mochila',10,'mochila.jpg',1)");
+
+            mb.Sql("Insert into Products(Name, Price, Description, Stock, ImageUrl, CategoryId) " +
+                "Values('Saia',29.90,'Saia',15,'saia.jpg',2)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from Products");
+        }
+    }
+}
